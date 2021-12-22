f0bdf2b9856eb368c9a49b30952eb7130f34abe8
dai10dai12 <95325471+dai10dai12@users.noreply.github.com>
2021-12-22 18:40:02 +0900

    ストロングパラメーターの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,15 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  def new
+    @tweets = Tweet.new
+  end
+  def create
+    Tweets.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
